119a9644238ea18a2b3e041fe9447a64412c5154
Stephen Pate <srp0023@uah.edu@gmail.com>
2020-02-08 23:52:19 -0600

    Created most of the methods for Song class. The 'Order' field probably needs a random number generator (stub created). I created 2 methods to convert song length (stored in seconds) to minutes and remaining seconds (one method for each). Intent is to use them together. I started writing some tests for the methods over on Main; we'll delete those when they're no longer needed.

--- src/setlist/Main.java
+++ src/setlist/Main.java
@@ -4,5 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Team 3");
+
+        // Testing methods of Song class
+        Song test = new Song();
+        System.out.println(test.toString());
+        //Title
+        String temp = "This is a title. ";
+        test.setTitle(temp);
+        System.out.println(temp +  " | " + test.getTitle());
+        //Composer
+        temp = "This is a composer. ";
+        test.setComposer(temp);
+        System.out.println(temp +  " | " + test.getComposer());
+        //Key
+        temp = "A maj";
+        test.setKey(temp);
+        System.out.println(temp + " | " + test.getKey());
+        //Archive
+        System.out.println("Should be False: " + test.isArchive());
+        test.setArchive(true);
+        System.out.println("Should now be True: " + test.isArchive());
+        test.toggleArchive();
+        System.out.println("Should now be False: " + test.isArchive());
+        test.toggleArchive();
+        System.out.println("Should now be True: " + test.isArchive());
+        //Length
+        int i = 135;
+        test.setLength(i);
+        System.out.println(i + " | " + test.getLength());
+        System.out.println(test.getLengthMin() + ":" + test.getLengthSec());
     }
 }
--- src/setlist/Song.java
+++ src/setlist/Song.java
@@ -13,6 +13,17 @@ public class Song {
     private int Intro; // length of introduction in seconds
     private int Order; // for randomized sorting
 
+    public Song() {
+       Title = "";
+       Composer = "";
+       Key = "";
+       Archive = FALSE;
+       Length = 0;
+       Tempo = 0;
+       Intro = 0;
+       Order = -1;
+    }
+
     /**
      * @param title Title of the song.
      */
@@ -59,7 +70,7 @@ public class Song {
      * @return Returns true if archived
      */
     public boolean isArchive() {
-        return Archive;
+        return this.Archive;
     }
 
     /**
@@ -73,11 +84,100 @@ public class Song {
      * Switches Archive marker to TRUE if FALSE, or FALSE if TRUE
      */
     public void toggleArchive(){
-        if (this.Archive){
+        if (this.Archive) {
             this.Archive = FALSE;
         }
         else if (this.Archive == FALSE){
             this.Archive = TRUE;
         }
+    }
+
+    /**
+     * @param intro Length of introduction in seconds
+     */
+    public void setIntro(int intro) {
+        this.Intro = intro;
+    }
+
+    /**
+     * @return Returns intro length in seconds
+     */
+    public int getIntro(){
+        return this.Intro;
+    }
+
+    /**
+     * @param length Total length of song in seconds
+     */
+    public void setLength(int length) {
+        this.Length = length;
+    }
+
+    /**
+     * @return Returns total song length in seconds
+     */
+    public int getLength() {
+        return this.Length;
+    }
+
+    /**
+     * @return Returns whole minutes of the song
+     */
+    public int getLengthMin(){
+        return (this.Length / 60);
+    }
+
+    /**
+     * @return Returns remaining seconds of song. Use with getLengthMin for total song length
+     */
+    public int getLengthSec(){
+        return (this.Length % 60);
+    }
+
+    /**
+     * @param tempo Song tempo in BPM
+     */
+    public void setTempo(int tempo) {
+        this.Tempo = tempo;
+    }
+
+    /**
+     * @return Returns tempo in BPM
+     */
+    public int getTempo() {
+        return this.Tempo;
+    }
+
+    /**
+     * @param order Sets list order
+     */
+    public void setOrder(int order) {
+        this.Order = order;
+    }
+
+    /**
+     * @return Returns list order
+     */
+    public int getOrder() {
+        return this.Order;
+    }
+
+    public void setOrderRandom(){
+        System.out.println("Not implemented");
+        //Should set order to a random number
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "Title='" + Title + '\'' +
+                ", Composer='" + Composer + '\'' +
+                ", Key='" + Key + '\'' +
+                ", Archive=" + Archive +
+                ", Length=" + Length +
+                ", Tempo=" + Tempo +
+                ", Intro=" + Intro +
+                ", Order=" + Order +
+                '}';
     }
 }
