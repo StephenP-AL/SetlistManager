ff019bc1fb30d6a16b405009bcab21bb26909a3c
Paul Zorovich <pjz0001@uah.edu>
2020-03-02 20:07:25 -0600

    Added parameterized constructor for TitleView class.

--- src/setlist/TitleView.java
+++ src/setlist/TitleView.java
@@ -4,4 +4,15 @@ package setlist;
  * A view of the catalog that sorts the Catalog by song title
  */
 public class TitleView {
+
+    Catalog alphabeticalCatalog;
+
+    /**
+     * Parameterized constructor
+     */
+    public TitleView(Catalog catalog)
+    {
+        alphabeticalCatalog = catalog.duplicate();
+        alphabeticalCatalog.sortTitle();
+    }
 }
